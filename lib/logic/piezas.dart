a44eba3bd92254cd23ae5b2789b2c2194234059b
Germán López Pérez <germlopezperez@gmail.com>
2025-05-25 22:09:12 +0200

    Update piezas.dart

--- lib/logic/piezas.dart
+++ lib/logic/piezas.dart
@@ -1,33 +1,20 @@
 abstract class Pieza {
-  final String nombre;
-  final double precio;
-
-  Pieza(this.nombre, this.precio);
-
-  // factory Pieza.fromJson(Json json) {
-  //   final rawPrecio = json['precio'];
-  //   double parsedPrecio;
-  //   if (rawPrecio is num) {
-  //     parsedPrecio = rawPrecio.toDouble();
-  //   } else if (rawPrecio is String) {
-  //     parsedPrecio = double.tryParse(rawPrecio) ?? 0.0;
-  //   } else {
-  //     parsedPrecio = 0.0;
-  //   }
-  //   return Pieza(
-  //     nombre: json['nombre'] as String,
-  //     tipo: json['tipo'] as String,
-  //     precio: parsedPrecio,
-  //   );
-  // }
-
-}
-
-class CPU extends Pieza {
   final String modelo;
   final double precio;
 
-  CPU({required this.modelo, required this.precio});
+  const Pieza({required this.modelo, required this.precio});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'modelo': modelo,
+      'precio': precio,
+    };
+  }
+}
+
+class CPU extends Pieza {
+  const CPU({required String modelo, required double precio})
+      : super(modelo: modelo, precio: precio);
 
   factory CPU.fromJson(Map<String, dynamic> json) {
     return CPU(
@@ -35,72 +22,35 @@ class CPU extends Pieza {
       precio: (json['precio'] as num).toDouble(),
     );
   }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'modelo': modelo,
-      'precio': precio,
-    };
-  }
 }
 
-
 class RAM extends Pieza {
-  final String capacidad;
-  final double precio;
-
-  RAM({required this.capacidad, required this.precio});
+  const RAM({required String modelo, required double precio})
+      : super(modelo: modelo, precio: precio);
 
   factory RAM.fromJson(Map<String, dynamic> json) {
     return RAM(
-      capacidad: json['capacidad'],
+      modelo: json['modelo'],
       precio: (json['precio'] as num).toDouble(),
     );
   }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'capacidad': capacidad,
-      'precio': precio,
-    };
-  }
 }
 
-
 class Almacenamiento extends Pieza {
-  final String tipo;
-  final String capacidad;
-  final double precio;
-
-  Almacenamiento({
-    required this.tipo,
-    required this.capacidad,
-    required this.precio,
-  });
+  const Almacenamiento({required String modelo, required double precio})
+      : super(modelo: modelo, precio: precio);
 
   factory Almacenamiento.fromJson(Map<String, dynamic> json) {
     return Almacenamiento(
-      tipo: json['tipo'],
-      capacidad: json['capacidad'],
+      modelo: json['modelo'],
       precio: (json['precio'] as num).toDouble(),
     );
   }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'tipo': tipo,
-      'capacidad': capacidad,
-      'precio': precio,
-    };
-  }
 }
 
-
 class GPU extends Pieza {
-  final String modelo;
-  final double precio;
-
-  GPU({required this.modelo, required this.precio});
+  const GPU({required String modelo, required double precio})
+      : super(modelo: modelo, precio: precio);
 
   factory GPU.fromJson(Map<String, dynamic> json) {
     return GPU(
@@ -108,12 +58,4 @@ class GPU extends Pieza {
       precio: (json['precio'] as num).toDouble(),
     );
   }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'modelo': modelo,
-      'precio': precio,
-    };
-  }
 }
-
